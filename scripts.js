91bdaa21932eba9e55d7bf2d927c7c2488487709
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2023-07-12 13:07:16 -0400

    Fix form layout/code format

---
+++ scripts.js
@@ -0,0 +1,26 @@
+const first_name = document.getElementById('first_name')
+const last_name = document.getElementById('last_name')
+const email = document.getElementById('email')
+const phone_number = document.getElementById('phone_number')
+const password = document.getElementById('password')
+const confirm_password = document.getElementById('confirm_password')
+
+const form = document.getElementById('sign-up-form')
+
+const errorMessage = document.getElementById('error')
+
+form.addEventListener('submit', (e) => {
+    let messages = []
+    if (first_name.value === '' || first_name.value == null) {
+        messages.push('Name is required')
+    }
+
+    if (password.value !== confirm_password.value) {
+        messages.push('Passwords do not match!')
+    }
+
+    if (messages.length > 0) {
+        e.preventDefault()
+        errorMessage.innerText = messages.join(',')
+    }
+})
